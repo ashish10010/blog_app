9247fec392dbdca45da23a029344c7998ff7a96a
Ashish Kunwar <thaladipesh89@gmail.com>
2025-01-18 20:46:20 +0545

    blog feature initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:blog_app/features/blog/presentation/pages/blog_page.dart';
 import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -48,13 +49,7 @@ class _MyAppState extends State<MyApp> {
         },
         builder: (context, isLoggedIn) {
           if (isLoggedIn) {
-            return Scaffold(
-              body: Center(
-                child: Text(
-                  "Implement Homepage",
-                ),
-              ),
-            );
+            return const BlogPage();
           }
           return const SignInPage();
         },
