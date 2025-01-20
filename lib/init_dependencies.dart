4afab3b412f0d38ef22d615810bdbe7e22d853f9
Ashish Kunwar <thaladipesh89@gmail.com>
2025-01-20 20:22:14 +0545

    uploading blog , dependecy inversion and form validation

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -6,6 +6,11 @@ import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/secrets/app_secrets.dart';
@@ -14,6 +19,7 @@ final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
   _initAuth();
+  _initBlog();
   final supabase = await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
@@ -64,4 +70,24 @@ void _initAuth() {
         appUserCubit: serviceLocator<AppUserCubit>(),
       ),
     );
+}
+
+void _initBlog() {
+  //datasources
+  serviceLocator
+    ..registerFactory<BlogRemoteDataSource>(() => BlogRemoteDataSourceImpl(
+          serviceLocator(),
+        ))
+    //repository
+    ..registerFactory<BlogRepository>(() => BlogRepositoryImpl(
+          serviceLocator(),
+        ))
+    //usecase
+    ..registerFactory(() => UploadBlog(
+          serviceLocator(),
+        ))
+    //blogbloc
+    ..registerLazySingleton(() => BlogBloc(
+          serviceLocator<UploadBlog>(),
+        ));
 }
