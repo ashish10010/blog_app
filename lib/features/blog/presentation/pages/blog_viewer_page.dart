99023e9cfb0afab1a8dd6975dbe671baf545056e
Ashish Kunwar <thaladipesh89@gmail.com>
2025-01-21 18:43:12 +0545

    date formating using intl plus scroll bar

--- lib/features/blog/presentation/pages/blog_viewer_page.dart
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/utils/calculate_reading_time.dart';
+import 'package:blog_app/core/utils/format_date.dart';
 import 'package:flutter/material.dart';
 
 import '../../domain/entities/blog.dart';
@@ -18,58 +19,60 @@ class BlogViewerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                blog.title,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'By ${blog.posterName}',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                '${blog.updatedAt} . ${calculateReadingTime(blog.content)} minutes.',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: AppPallete.greyColor,
+                Text(
+                  'By ${blog.posterName}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(blog.imageUrl),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                blog.content,
-                style: TextStyle(
-                  fontSize: 16,
-                  height: 2,
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-            ],
+                Text(
+                  '${formatDatebydMMMYYYY(blog.updatedAt)} . ${calculateReadingTime(blog.content)} minutes.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: AppPallete.greyColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(blog.imageUrl),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  blog.content,
+                  style: TextStyle(
+                    fontSize: 16,
+                    height: 2,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
